0ea806456334dd81dd5ec1c71cc0ea7d1308c142
escakot <ec@neorennie.com>
2017-07-12 12:55:40 -0400

    Initial Commit

---
+++ InstaKilo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaKilo
+//
+//  Created by Errol Cheong on 2017-07-12.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
